cf3e5075981b282d18cf36b2ae9e18ffd0caa51e
T-A-V-I <t-a-v-i@T-A-V-I-MacBook-M1.local>
2023-11-18 14:57:50 +0200

    first commit

---
+++ JS/script.js
@@ -0,0 +1,81 @@
+//togle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+};
+
+// scroll sections
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      // active navbar link
+      navLinks.forEach(links => {
+        links.classList.remove('active');
+        document
+          .querySelector('header nav a[href*=' + id + ']')
+          .classList.add('active');
+      });
+    }
+  });
+  // sticky header
+  let header = document.querySelector('header');
+
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+  // remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
+
+  //animation footer on scroll
+  let footer = document.querySelector('footer');
+
+  footer.classList.toggle(
+    'show-animate',
+    this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight
+  );
+};
+
+// Scroll Reveal
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+  var reveals = document.querySelectorAll('.reveal');
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if (revealtop < windowheight - revealpoint) {
+      reveals[i].classList.add('active');
+    } else {
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+// Project Slider
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MyPortofolio
+# MyPortofolio

4ba4f6d6a18d88d1dba991c91df5a729479c0748
T-A-V-I <t-a-v-i@T-A-V-I-MacBook-M1.local>
2024-02-03 15:58:23 +0200

    Form update

--- JS/script.js
+++ JS/script.js
@@ -79,3 +79,32 @@ var swiper = new Swiper('.mySwiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+//Form deploy
+
+const form = document.querySelector('form');
+const fullName = document.getElementById('name');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+
+function sendEmail() {
+  const bodyMessage = `Full Name: ${fullName.value} </br> Email: ${email.value} </br> Phone Number: ${phone.value} </br> Message: ${message.value}`;
+
+  Email.send({
+    Host: 'smtp.elasticemail.com',
+    Username: 'it.t4vi@gmail.com',
+    Password: 'B34810551A46A2EFC468E07C1E31A435F7E6',
+    To: 'it.t4vi@gmail.com',
+    From: 'it.t4vi@gmail.com',
+    Subject: subject.value,
+    Body: bodyMessage,
+  }).then(message => alert(message));
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  sendEmail();
+});

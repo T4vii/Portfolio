a55c107395d06ea636297728e143e97b76208b81
T-A-V-I <t-a-v-i@T-A-V-I-MacBook-M1.local>
2024-02-03 16:02:18 +0200

    Form update

--- JS/script.js
+++ JS/script.js
@@ -90,7 +90,7 @@ const subject = document.getElementById('subject');
 const message = document.getElementById('message');
 
 function sendEmail() {
-  const bodyMessage = `Full Name: ${fullName.value} </br> Email: ${email.value} </br> Phone Number: ${phone.value} </br> Message: ${message.value}`;
+  const bodyMessage = `Full Name: ${fullName.value}<br> Email: ${email.value}<br> Phone Number: ${phone.value}<br> Message: ${message.value}`;
 
   Email.send({
     Host: 'smtp.elasticemail.com',
